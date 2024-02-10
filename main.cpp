caf5cb5a2a7d957ca9c6d6a5bd6e000ae3031bb5
karocern <karoliscernevicius@gmail.com>
2024-02-10 18:39:49 +0200

    Pirmas punktas ( Tik Vidurkis (Be User Debugginimo))

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
 struct stud{
-    string vard, pav;                                                                // saugom studentu varda ir pavarde
+    string vard, pav;                                                                       // saugom studentu varda ir pavarde
     int nd, egz = 0;                                                                       // saugom tarpini namu darbu rezultata, egzamino rezultata
     double vidurkis = 0;
 };
 
-int main(){
-    int s = 0, n = 0;                                                                          //kiek studentu ir namu darbu
-    stud *obj = new stud[s];
-    delete[] obj;
-    cout << "Iveskite studentu kieki" <<endl;
-    cin >> s;
-    cout << "Iveskite namu darbu uzduociu kieki"<<endl;
-    cin >> n;
-    obj = new stud[s];
+void Read (stud* obj, int n, int s){
     for(int i = 0; i<s; i++){
-        cout << "Iveskite studento varda" <<endl;
+        cout << "Iveskite studento varda: " <<endl;
         cin >> obj[i].vard;
-        cout << endl;
-        cout << "Iveskite studento pavarde" <<endl;
+        cout << "Iveskite studento pavarde: " <<endl;
         cin >> obj[i].pav;
-        cout << endl;
-        cout << "Iveskite " << n << " studento namu darbu pazymiu"<<endl;
+        cout << "Iveskite " << n << " studento namu darbu pazymius: "<<endl;
         for(int j = 0; j<n; j++){
             cin >> obj[i].nd;
             obj[i].vidurkis += obj[i].nd;
@@ -34,18 +25,33 @@ int main(){
         cout << "Iveskite egzamino pazymi"<<endl;
         cin >> obj[i].egz;
         obj[i].vidurkis = obj[i].vidurkis * 0.4;
-        cout << obj[i].vidurkis <<endl;
         obj[i].vidurkis = (double)obj[i].egz * 0.6 + obj[i].vidurkis;
     }
-        for(int i = 0; i<s; i++){
-        cout << "Studento vardas" <<endl;
-        cout << obj[i].vard<<endl;
-        cout << "Studento pavarde" <<endl;
-        cout << obj[i].pav;
-        cout << endl;
-        cout << "Studento vidurkis"<<endl;
-        cout << obj[i].vidurkis;
+}
+
+void Print (stud* obj, int s){
+
+    cout << left << setw(10) << "Vardas"  << setw(15) << "Pavarde" << setw(15) << left << "Galutinis (Vid.)" << endl;
+    for (int z = 0; z<50; z++){ cout << '-'; }
+    cout << endl;
+        
+    for(int i = 0; i<s; i++){
+        cout << left << setw(10) << obj[i].vard << setw(15) << obj[i].pav << setw(15) << left << setprecision(3) << obj[i].vidurkis << endl;
     }
+}
+
+int main(){
+    int s = 0, n = 0;                                                                          //kiek studentu ir namu darbu
+    stud *obj = new stud[s];
+    delete[] obj;
+    cout << "Iveskite studentu skaiciu" <<endl;
+    cin >> s;
+    cout << "Iveskite namu darbu uzduociu skaiciu: "<<endl;
+    cin >> n;
+    obj = new stud[s];
+    Read(obj, n, s);
+    Print(obj, s);
+
     delete[] obj;
     return 0;
 
