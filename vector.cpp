1127929103fa2d344fb0b481d98a2781f4096283
karocern <karoliscernevicius@gmail.com>
2024-02-21 20:32:14 +0200

    Added output to file function

--- vector.cpp
+++ vector.cpp
@@ -32,6 +32,7 @@ int ExamParameters();
 void ClearCin();
 
 void readFile(vector<stud> &obj, const string filename);
+void PrintFile(vector<stud> &obj);
 
 vector <string> Names {"Audrius", "Edvard", "Ganesh", "Nojus", "Cleophas", "Rodrigo","Jurgita", "Ugne", "Tatiana", "Sarah"};
 vector <string> Surnames {"Czerniewicz", "Finch", "Hummel", "McKowen", "Warszawski", "Clery", "Wilbur", "Kennedy", "Nixon", "Obama"};
@@ -39,10 +40,62 @@ vector <string> Surnames {"Czerniewicz", "Finch", "Hummel", "McKowen", "Warszaws
 int main(){
     vector<stud> obj;
     string filename = "kursiokai.txt";
-    readFile(obj, filename);
 
-    Assign(obj);
-    Print(obj);
+    int option = 0;
+    bool pass = true;
+
+    while(pass){
+    cout << " Pasrinkite programos veikimo eigą: " << endl;
+    cout << " (1) Duomenis nuskaityti iš failo. " << endl; 
+    cout << " (2) Duomenis suvesti programoje. " << endl;
+    cin >> option;
+    ClearCin();
+
+    switch (option){
+        case 1 :{
+            readFile(obj, filename);
+            pass = false;
+            break;
+        }
+        case 2 :{
+            Assign(obj);
+            pass = false;
+            break;
+        }
+        default:{
+            cout << " Netinkamas įvesties formatas, bandykite dar kartą :   " << endl;
+            break;
+        }
+    }
+    }
+
+    pass = true;
+
+    while(pass){
+
+    cout << " Pasrinkite duomenų spausdinimo būdą: " << endl;
+    cout << " (1) Duomenis spausdinti į failą. (Rekomenduojama dideliems kiekiams duomenų) " << endl; 
+    cout << " (2) Duomenis spausdinti programoje. " << endl;
+    cin >> option;
+    ClearCin();
+
+    switch (option){
+        case 1 :{
+            PrintFile(obj);
+            pass = false;
+            break;
+        }
+        case 2 :{
+            Print(obj);
+            pass = false;
+            break;
+        }
+        default:{
+            cout << " Netinkamas įvesties formatas, bandykite dar kartą :   " << endl;
+            break;
+        }
+    }
+    }
     return 0;
 
 }
@@ -96,10 +149,13 @@ void RandomNames(vector<stud> &obj, int counter){
 void Assign(vector<stud> &obj){
 
     bool pass = true;
+    bool program = true;
     int counter = 0;
     char c = ' ';
     int option = 0;
 
+    while(program){
+    
     cout << " Pasrinkite studento pridėjimo eigą: " << endl;
     cout << " (1) Duomenis suvesti ranka. " << endl; 
     cout << " (2) Atsitiktinai generuoti tik studentų pažymius" << endl;
@@ -111,6 +167,7 @@ void Assign(vector<stud> &obj){
 
     switch (option){
         case (1):{
+            program = false;
             while(pass){
                 cout << " Ar norite pridėti studentą? (Y/N) " << endl;
                 cin >> c;
@@ -173,14 +230,18 @@ void Assign(vector<stud> &obj){
             }
             break;
         }
-        case (2):{
+        case (2):
+        {
+            program = false;
             cout << " Įveskite namų darbų skaičių:  " << endl;
             int numberOfHw = 0;
             numberOfHw = StartParameters();
             while(pass){
+
                 cout << " Ar norite pridėti studentą? (Y/N) " << endl;
                 cin >> c;
                 ClearCin();
+
                 switch (c){
                     case 'Y':
                     {
@@ -222,7 +283,9 @@ void Assign(vector<stud> &obj){
             }
             break;
         }
-        case (3):{
+        case (3):
+        {
+            program = false;
             cout << " Įveskite studentų skaičių:  " << endl;
             int numberOfSt = 0;
             numberOfSt = StartParameters();
@@ -245,7 +308,9 @@ void Assign(vector<stud> &obj){
             }
             break;
         }
-        case (4):{
+        case (4):
+        {
+            program = false;
             cout << " Programos darbas baigiamas " << endl;
             exit(0);
             break;
@@ -256,14 +321,20 @@ void Assign(vector<stud> &obj){
         }
     }
 }
+}
 
 
 void Print (vector<stud> &obj){
     int temp;
+    bool pass = true;
     int s = obj.size();
+
+    while(pass){
+    
     cout << endl << endl << " Pasirinkite galutinio rezultato pateikimo būdą : " << endl;
     cout << " (1) Namų darbų vidurkis. " << endl; 
     cout << " (2) Namų darbų mediana." << endl;
+    cout << " (3) Baigti darbą " << endl;
 
     cin >> temp;
     ClearCin();
@@ -271,6 +342,7 @@ void Print (vector<stud> &obj){
     switch(temp){
         case (1):
         {
+
             cout << left << setw(26) << "Vardas"  << setw(26) << "Pavardė" << setw(15) << left << "Galutinis (Vid.)" << endl;
             for (int z = 0; z<70; z++){ cout << '-'; }
             cout << endl;
@@ -289,14 +361,19 @@ void Print (vector<stud> &obj){
             }
             break;
         }
+        case (3):
+        {
+            cout << " Programos darbas baigiamas " << endl;
+            exit(0);
+            break;
+        }
         default:
         {
             cout << " Pateikėte netinkamą simbolį, bandykite iš naujo" << endl;
-            Print(obj);
             break;
         }
     }
-    
+    }
 }
 
 int StartParameters(){
@@ -386,10 +463,8 @@ void readFile(vector<stud> &obj, const string filename){
 
     ifstream file(filename);
     if (!file.is_open()){
-
         cerr << "Error: Unable to open file " << filename << endl;
         exit(0);
-
     }
     string line;
     getline(file, line);
@@ -398,7 +473,6 @@ void readFile(vector<stud> &obj, const string filename){
         obj.resize(counter+1);
         istringstream iss(line);
         if (!(iss >> obj.at(counter).vard >> obj.at(counter).pav)){
-
             cerr << "Error reading student name and surname from file " << filename << endl;
             continue;
         }
@@ -436,4 +510,69 @@ void readFile(vector<stud> &obj, const string filename){
         }
         counter++;
     }
+    ifstream fclose(filename);
 }
+
+void PrintFile(vector<stud> &obj){
+
+    ofstream PrintOut("output.txt");
+    if (!PrintOut.is_open())
+    {
+        cerr << "Error: Unable to open output file." << endl;
+        return;
+    }
+
+    int temp;
+    bool pass = true;
+    int s = obj.size();
+
+    while(pass){
+    
+    cout << endl << endl << " Pasirinkite galutinio rezultato pateikimo būdą : " << endl;
+    cout << " (1) Namų darbų vidurkis. " << endl; 
+    cout << " (2) Namų darbų mediana." << endl;
+    cout << " (3) Baigti darbą " << endl;
+
+    cin >> temp;
+    ClearCin();
+
+    switch(temp){
+        case (1):
+        {
+            PrintOut << left << setw(26) << "Vardas"  << setw(26) << "Pavardė" << setw(15) << left << "Galutinis (Vid.)" << endl;
+            for (int z = 0; z<70; z++){ PrintOut << '-'; }
+            PrintOut << endl;
+            for(int i = 0; i<s; i++){
+                PrintOut << left << setw(26) << obj.at(i).vard << setw(26) << obj.at(i).pav << setw(15) << left << fixed << setprecision(2) << obj.at(i).FinalAverage << endl;
+            }
+            pass = false;
+            break;
+        }
+        case (2):
+        {
+            PrintOut << left << setw(26) << "Vardas"  << setw(26) << "Pavardė" << setw(15) << left << "Galutinis (Med.)" << endl;
+            for (int z = 0; z<70; z++){ PrintOut << '-'; }
+            PrintOut << endl;
+            for(int i = 0; i<s; i++){
+                PrintOut << left << setw(26) << obj.at(i).vard << setw(26) << obj.at(i).pav << setw(15) << left << fixed << setprecision(2) << obj.at(i).median << endl;
+            }
+            pass = false;
+            break;
+        }
+        case (3):
+        {
+            cout << " Programos darbas baigiamas " << endl;
+            exit(0);
+            break;
+        }
+        default:
+        {
+            cout << " Pateikėte netinkamą simbolį, bandykite iš naujo" << endl;
+            break;
+        }
+    }
+    }
+
+    PrintOut.close();
+}
+
